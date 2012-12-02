582ba3bba49841bc75800944273e1cd087964b65
Stanislav Cherkasov <stanich00@gmail.com>
2012-12-02 21:20:10 +0400

    fixed errors of logging

--- tests/TestSimpleClient.py
+++ tests/TestSimpleClient.py
@@ -2,16 +2,27 @@
 
 import socket
 import time
+import threading
+from threading import Thread
 
-HOST = '127.0.0.1'    
-PORT = 10000          
-s = socket.socket(socket.AF_INET,  socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+def TestConnect():
+	print hex(threading.current_thread().ident), " started"
+	HOST = '127.0.0.1'    
+	PORT = 10000          
+	s = socket.socket(socket.AF_INET,  socket.SOCK_STREAM)
+	s.connect((HOST, PORT))
 
-msg = "Hello"
-s.send(msg)
-print "Sent: ", msg
+	msg = "Hello"
+	s.send(msg)
+	print "Sent: ", msg
 
-data = s.recv(1024)
-s.close()
-print "Recieved: ", repr(data)
+	data = s.recv(1024)
+	s.close()
+	print "Recieved: ", repr(data)
+	print hex(threading.current_thread().ident), " finished"
+
+
+# start here
+for i in range(100):
+	t = Thread(target=TestConnect)
+	t.start()
